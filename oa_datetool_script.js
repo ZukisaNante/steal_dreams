f6af711089b2b64ec1344277cf4deeee7e821523
ZukisaNante <zukisanante@gmail.com>
2019-10-24 15:25:55 +0200

    js updated

--- oa_datetool_script.js
+++ oa_datetool_script.js
@@ -3,12 +3,13 @@
 $(document).ready(function() {
     console.log("ready");
     todaysDate();
+    deliveryDays();
+    delivery();
     $("#hider").hide(); // hides count div
     $("a.show").on("click", function() {
         $("#hider").toggle();
-        $("#leverbaar.show").on("click", function() {
-            $("#leverbaar").val(n_days);
-        });
+
+        // $(".leverbaar").val(n_days);
     });
     // check user input
     $("#werkdagen").keyup(function() {
@@ -60,15 +61,50 @@ function deliveryDays() {
         if (start.getUTCDay()) n_days = n_days + 1; // not sunday
         start.setUTCHours(24); // add a day
     }
-    $("#werkdagen").html(n_days);
+    $("p.leverbaar").html(
+        "Uw product zal binnen" + " " + n_days + " " + "dagen geleveerd worden!"
+    );
     console.log(n_days); // 12
 }
 
 // subtracting days from the input field
-/* $(function() {
-    $("#werkdagen, .b2").on("keydown keyup", sub);
 
-    function sub() {
-        $(".c2").val(Number($("#werkdagen").val()) - Number($(".b2").val()));
+/* $(document).ready(function() {
+    //this calculates values automatically
+    sum();
+    $("#num1, #num2").on("keydown keyup", function() {
+        sum();
+    });
+});
+
+function sum() {
+    var num1 = document.getElementById("werkdagen").value;
+    var num2 = document.getElementsByClassName("leverbaar").value;
+    //var result = parseInt(num1) + parseInt(num2);
+    var result1 = parseInt(num2) - parseInt(num1);
+    if (!isNaN(result)) {
+        document.getElementById("sum").value = result;
+        document.getElementById("subt").value = result1;
     }
-}); */
+} */
+
+// Function delivery date 2
+function delivery() {
+    var today = new Date(); //
+    var business_days = $("#werkdagen").val();
+    console.log(business_days);
+
+    var deliveryDate = today; //will be incremented by the for loop
+    var total_days = business_days; //will be used by the for loop
+    for (var days = 1; days <= total_days; days++) {
+        deliveryDate = new Date(today.getTime() + days * 24 * 60 * 60 * 1000);
+        if (deliveryDate.getDay() == 0 || deliveryDate.getDay() == 6) {
+            //it's a weekend day so we increase the total_days of 1
+            total_days++;
+        }
+    }
+
+    $("p.leverbaar").html("Uw product kan geleverd worden op" + deliveryDate);
+    console.log(today);
+    console.log(deliveryDate);
+}
