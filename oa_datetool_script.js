160f0cc8dc4d30a164cb00e9434a09429fb21c2e
ZukisaNante <zukisanante@gmail.com>
2019-10-27 22:13:43 +0100

    js updated

--- oa_datetool_script.js
+++ oa_datetool_script.js
@@ -3,33 +3,17 @@
 $(document).ready(function() {
     console.log("ready");
     todaysDate();
-    deliveryDays();
+    // deliveryDays();
     delivery();
     $("#hider").hide(); // hides count div
-    $(".me").hide(); // hide button
+
     $("a.show").on("click", function() {
-        $("#hider").toggle();
-        $(".me").toggle(); // show button
+        $("#hider").show();
     });
     // check if user input changed
     $("input#werkdagen").change(function(e) {
-        alert(e.target.value);
-    });
-    // show input value in the form
-    $(".showHider").submit(function(e) {
-        e.preventDefault();
-        var dagen = $("input#werkdagen").val();
-        console.log(dagen);
-    });
-
-    //input change
-    $(document).on("change", "input", function() {
-        $("submit").on("click")(function() {
-            var secondEstimate = $("input").val();
-            var test = secondEstimate;
-
-            console.log(test);
-        });
+        // alert(e.target.value);
+        delivery();
     });
 });
 
@@ -54,60 +38,62 @@ function todaysDate() {
     var date = new Date();
 }
 
-// RETURNING DELIVERY DAYS
-//                    yyyy-MM-dd hh:mm:ss
-function deliveryDays() {
-    var start = new Date("2019-10-25T00:00:00Z"),
-        end = new Date("2019-11-10T00:00:00Z"),
-        holiday = [
-            ["New Years Day", [1, 1]],
-            ["Easter Monday", [22, 4]],
-            ["Christmas Day", [12, 25]],
-            ["Labour Day", [30, 5]],
-            ["Pentecost Monday", [10, 6]],
-            ["Belgium National Day ", [21, 6]],
-            [" Assumption Day ", [15, 8]],
-            ["Presidents Day ", [3, 1, 2]],
-            ["All Saints’ Day ", [1, 11]],
-            ["Armistice Day", [11, 11]]
-        ];
-    /*  holiday = [
-                                  new Date("2019-10-27T00:00:00Z"),
-                                  new Date("2030-05-03T00:00:00Z")
-                              ], */
-    (i = holiday.length), (n_days = 0);
-    while (i--) {
-        // loop over holidays
-        if (holiday[i] >= start)
-            if (holiday[i] <= end) n_days = n_days - 1; // day holiday within dates
-    }
-    while (start <= end) {
-        if (start.getUTCDay()) n_days = n_days + 1; // not sunday
-        start.setUTCHours(24); // add a day
-    }
-    $("p.deliver").html(
-        "Uw product kan binnen" + " " + n_days + " " + "dagen geleveerd worden!"
-    );
-    console.log(n_days); // 12
-}
-
 // Function delivery date 2
 function delivery() {
     var today = new Date(); //
     var business_days = $("#werkdagen").val();
     console.log(business_days);
-
     var deliveryDate = today; //will be incremented by the for loop
     var total_days = business_days; //will be used by the for loop
     for (var days = 1; days <= total_days; days++) {
         deliveryDate = new Date(today.getTime() + days * 24 * 60 * 60 * 1000);
         if (deliveryDate.getDay() == 0 || deliveryDate.getDay() == 6) {
             //it's a weekend day so we increase the total_days of 1
+            // holidays
             total_days++;
         }
     }
-    $("p.leverbaar").html("Uw product kan geleverd worden op  " + deliveryDate);
-    console.log(today);
+
+    $("p.leverbaar").html(
+        "Uw product kan geleverd worden op  " +
+        deliveryDate.toLocaleDateString("en-GB")
+    ); // better with "toDateString()" i think
+    // console.log(today);
+    //  console.log(deliveryDate);
+}
+
+// TEST HOLIDAY FUNCTION
+
+function holiday() {
+    var holiday = [
+        ["New Years Day", [1, 1]],
+        ["Easter Monday", [22, 4]],
+        ["Christmas Day", [12, 25]],
+        ["Labour Day", [30, 5]],
+        ["Pentecost Monday", [10, 6]],
+        ["Belgium National Day ", [21, 6]],
+        [" Assumption Day ", [15, 8]],
+        ["Presidents Day ", [3, 1, 2]],
+        ["All Saints’ Day ", [1, 11]],
+        ["Armistice Day", [11, 11]]
+    ];
+
+    for (var i in holiday) {
+        deivery();
+        if (
+            holiday[i] >= deliveryDate.getDay() == 6 &&
+            deliveryDate.getDay() == 0
+        ) {
+            // Check if specific holyday is Saturday or Sunday
+            var yourDate = new Date(holidays[i]);
+            if (yourDate.getDay() == 6 || yourDate.getDay() == 0) {
+                // If it is.. do nothing
+            } else {
+                // if it is not, reduce a day..
+                total_days--;
+                console.log(total_days);
+            }
+        }
+    }
     console.log(deliveryDate);
 }
-// onchange event lister
