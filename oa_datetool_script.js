3883861b3d0e63f1a903e88e4b4002a542365b4d
junior <zukisanante@gmail.com>
2019-10-31 16:26:52 +0100

    -a

--- oa_datetool_script.js
+++ oa_datetool_script.js
@@ -1,21 +1,39 @@
 // SCRIPT //
 
 $(document).ready(function() {
-    console.log("ready");
     todaysDate();
-    //formValidate();
-    // deliveryDays();
     delivery();
     $("#hider").hide(); // hides count div
     $(".startdatum-wrapper").hide(); // hides container rapper
+    $("#werkdagen").focus();
+    $("a.show").on("click", function(e) { 
+        if($("input#werkdagen").val() == ""){
+            e.preventDefault();
+     
+    //CALLED WHEN THE KEY IS PRESSED
+      $("#werkdagen").keypress(function (e) {
 
-    $("a.show").on("click", function() {
-        $("#hider").show();
-        //$("#showHider").valid();
+        //if the letter is not digit then display error and don't type anything
+        if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+
+           //display error message
+           $("#errmsg").html("Alleen Cijfers!").show().fadeOut("slow");
+                  return false;
+       }
+      }); } else { $("#hider").show();}
     });
-    // check if user input changed
-    $("input#werkdagen").change(function(e) {
-        // alert(e.target.value);
+
+    // CHECK IF USER INPUT CHANGED
+    $("input#werkdagen").change(function() {
+        $("#werkdagen").keypress(function (e) {
+            //if the letter is not digit then display error and don't type anything
+            if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+               //display error message
+               $("#errmsg").html("Alleen Cijfers!").show().fadeOut("slow");
+                      return false;
+           }
+          });
+
         delivery();
     });
 
@@ -35,13 +53,34 @@ $(document).ready(function() {
         $(".leverdatum-wrapper").show();
     });
     // INSERT INPUT VALUES INTO AN ARRAY WITH INVOEREN BUTTON CLICK
-    $("a.invoeren").on("click", function() {
-        inputDataValues();
-        //$(".userVerlof").valid();
+    $("a.invoeren").on("click", function(e) {
+        if($("input#jaar").val() == "" || $("input#maand").val() == "" || $("input#dag").val() == "" ){
+            e.preventDefault();
+            $("#jaar").keypress(function (e) {
+                if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+                   $("#errmsg").html("Alleen Cijfers!").show().fadeOut("slow");
+                          return false;
+               }
+              });
+              $("#maand").keypress(function (e) {
+                if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+                   $("#errmsg").html("Alleen Cijfers!").show().fadeOut("slow");
+                          return false;
+               }
+              });
+
+              $("#dag").keypress(function (e) {
+                if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+                   $("#errmsg").html("Alleen Cijfers!").show().fadeOut("slow");
+                          return false;
+               }
+              });
+
+        } else{
+            yearValidation();
+            inputDataValues();
+        }
     });
-
-    // USER INPUT VALIDATION
-
 });
 
 // FUNCTIONS //
@@ -67,6 +106,7 @@ function delivery() {
 
         //HOLIDAYS ARRAY
         var holiday = [new Date(currentYear + '-11-01'), new Date(currentYear + '-06-10'), new Date(currentYear + '-11-11'), new Date(currentYear + '-01-01'), new Date(currentYear + '-04-22'), new Date(currentYear + '-12-25'), new Date(currentYear + '-05-30'), new Date(currentYear + '-07-21'), new Date(currentYear + '-08-15'), new Date(currentYear + '-05-01')];
+       
         //GET DATE AND CHECK IF IT'S WEEKEND OR NOT, IF YES INCREMENT total_days +1
         deliveryDate = new Date(today.getTime() + days * 24 * 60 * 60 * 1000);
         if (deliveryDate.getDay() == 0 || deliveryDate.getDay() == 6) {
@@ -82,7 +122,6 @@ function delivery() {
 
         $('p.leverbaar').html("Uw product kan geleverd worden op: " + deliveryDate.toLocaleDateString("en-GB"));
         console.log("DELI // " + deliveryDate);
-
     }
 }
 
@@ -115,23 +154,58 @@ function inputDataValues() {
     console.log(inputValues['verlof_dag'], inputValues['verlof_maand'], inputValues['verlof_jaar']);
 }
 
-// FUNCTION USER INPUT VALIDATION
 
-// USER FORM INPUT VALIDATION
-/* function formValidate() {
-    $(".showHider").validate({
-        rules: {
-            werkdagen: 'required',
-            maand: 'required',
-            jaar: 'required',
-            dag: 'required',
-        },
-        messages: {
-            werkdagen: 'This field is required',
-            maand: 'This field is required',
-            jaar: 'This field is required',
-            dag: 'This field is required',
+ // Restricts input for each element in the set of matched elements to the given inputFilter.
+(function($) {
+    $.fn.inputFilter = function(inputFilter) {
+      return this.on("input keydown keyup mousedown mouseup select contextmenu drop", function() {
+        if (inputFilter(this.value)) {
+          this.oldValue = this.value;
+          this.oldSelectionStart = this.selectionStart;
+          this.oldSelectionEnd = this.selectionEnd;
+        } else if (this.hasOwnProperty("oldValue")) {
+          this.value = this.oldValue;
+          this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
         }
-    });
+      });
+    };
+  }(jQuery));
+  
+  
+  // INSTALL INPUT FILTERS
+  $("#werkdagen").inputFilter(function(value) {
+    return /^\d*$/.test(value); });
+    $("#jaar").inputFilter(function(value) {
+        return /^\d*$/.test(value); });
+        $("#dag").inputFilter(function(value) {
+            return /^\d*$/.test(value) && (value === "" || parseInt(value) <= 31); });
+  $("#maand").inputFilter(function(value) {
+    return /^\d*$/.test(value) && (value === "" || parseInt(value) <= 12); });
+  
 
-} */
+// YEAR VALIDATION
+function yearValidation(year,ev) {
+
+  var text = /^[0-9]+$/;
+  if(ev.type=="blur" || year.length==4 && ev.keyCode!=8 && ev.keyCode!=46) {
+    if (year != 0) {
+        if ((year != "") && (!text.test(year))) {
+
+            alert("Alleen Cijfers!");
+            return false;
+        }
+
+        if (year.length != 4) {
+            alert("Jaar is niet correct");
+            return false;
+        }
+        var current_year=new Date().getFullYear();
+        if((year < 1920) || (year > current_year))
+            {
+            alert("Het jaar moet tussen 1920 en het huidige jaar liggen");
+            return false;
+            }
+        return true;
+    } }
+}
+
